df3c09b1643c007a2d42d21f2426850f461a09c0
David Emanuel Sandoval <davidemanuelsandoval@gmail.com>
2022-06-06 20:16:56 -0300

    added allowed url

--- apitestapp/settings.py
+++ apitestapp/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,8 +26,10 @@ SECRET_KEY = 'django-insecure-)sl&txxyq3y%h26c+yb25o=^nc9kkw2*#4m^&0$zl0$zegb7*&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ENVIRONMENT = os.environ.get('ENVIRONMENT')
+ALLOWED_HOSTS = ['*.herokuapp.com']
+if ENVIRONMENT == 'local':
+    ALLOWED_HOSTS.append('*')
 
 # Application definition
 
